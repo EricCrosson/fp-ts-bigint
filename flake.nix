2a94c9a4e7d462d3d5cec88fae054afae567bdf2
Eric Crosson <eric.s.crosson@utexas.edu>
2022-11-26 16:31:29 -0600

    chore: use pre-commit-hooks.nix for pre-commit hooks
    - remove prettier
    - remove eslint

--- flake.nix
+++ flake.nix
@@ -2,11 +2,12 @@
   description = "fp-ts-bigint";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
@@ -14,26 +15,30 @@
     self,
     nixpkgs,
     flake-utils,
-    flake-compat,
-  }: (
+    pre-commit-hooks,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system: (
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          devShell = pkgs.mkShell {
-            name = "fp-ts-bigint shell";
-
-            packages = with pkgs; [
-              nodejs
-            ];
-
-            shellHook = ''
-              export PATH="$(pwd)/node_modules/.bin:$PATH"
-            '';
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+              prettier.enable = true;
+            };
           };
-        }
-      )
-    )
-  );
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+            ${checks.pre-commit-check.shellHook}
+          '';
+        };
+      }
+    );
 }
