9685467b869375ed0b8ce9b3aa257a1563337cd8
Eric Crosson <eric.s.crosson@utexas.edu>
2022-11-27 20:10:40 -0600

    chore: update script types
    which seem to have broken when we brought in a recent version of
    @types/node

--- scripts/FileSystem.ts
+++ scripts/FileSystem.ts
@@ -1,26 +1,48 @@
-import * as TE from 'fp-ts/TaskEither'
-import { flow } from 'fp-ts/function'
-import * as fs from 'fs'
-import G from 'glob'
+import * as TE from "fp-ts/TaskEither";
+import { flow } from "fp-ts/function";
+import * as fs from "fs";
+import G from "glob";
 
 export interface FileSystem {
-  readonly readFile: (path: string) => TE.TaskEither<Error, string>
-  readonly writeFile: (path: string, content: string) => TE.TaskEither<Error, void>
-  readonly copyFile: (from: string, to: string) => TE.TaskEither<Error, void>
-  readonly glob: (pattern: string) => TE.TaskEither<Error, ReadonlyArray<string>>
-  readonly mkdir: (path: string) => TE.TaskEither<Error, void>
-  readonly moveFile: (from: string, to: string) => TE.TaskEither<Error, void>
+  readonly readFile: (path: string) => TE.TaskEither<Error, string>;
+  readonly writeFile: (
+    path: string,
+    content: string
+  ) => TE.TaskEither<Error, void>;
+  readonly copyFile: (from: string, to: string) => TE.TaskEither<Error, void>;
+  readonly glob: (
+    pattern: string
+  ) => TE.TaskEither<Error, ReadonlyArray<string>>;
+  readonly mkdir: (path: string) => TE.TaskEither<Error, void>;
+  readonly moveFile: (from: string, to: string) => TE.TaskEither<Error, void>;
 }
 
-const readFile = TE.taskify<fs.PathLike, string, NodeJS.ErrnoException, string>(fs.readFile)
-const writeFile = TE.taskify<fs.PathLike, string, NodeJS.ErrnoException, void>(fs.writeFile)
-const copyFile = TE.taskify<fs.PathLike, fs.PathLike, NodeJS.ErrnoException, void>(fs.copyFile)
-const glob = TE.taskify<string, Error, ReadonlyArray<string>>(G)
-const mkdirTE = TE.taskify(fs.mkdir)
-const moveFile = TE.taskify<fs.PathLike, fs.PathLike, NodeJS.ErrnoException, void>(fs.rename)
+const readFile = TE.taskify<
+  fs.PathLike,
+  { encoding: BufferEncoding },
+  NodeJS.ErrnoException,
+  string
+>(fs.readFile);
+const writeFile = TE.taskify<fs.PathLike, string, NodeJS.ErrnoException, void>(
+  fs.writeFile
+);
+const copyFile = TE.taskify<
+  fs.PathLike,
+  fs.PathLike,
+  NodeJS.ErrnoException,
+  void
+>(fs.copyFile);
+const glob = TE.taskify<string, Error, ReadonlyArray<string>>(G);
+const mkdirTE = TE.taskify(fs.mkdir);
+const moveFile = TE.taskify<
+  fs.PathLike,
+  fs.PathLike,
+  NodeJS.ErrnoException,
+  void
+>(fs.rename);
 
 export const fileSystem: FileSystem = {
-  readFile: (path) => readFile(path, 'utf8'),
+  readFile: (path) => readFile(path, { encoding: "utf8" }),
   writeFile,
   copyFile,
   glob,
@@ -28,5 +50,5 @@ export const fileSystem: FileSystem = {
     mkdirTE,
     TE.map(() => undefined)
   ),
-  moveFile
-}
+  moveFile,
+};
